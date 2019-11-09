319c4ebe24efb68d815eded64390eef80abb331f
Byron Bustamante <byronbl.12@gmail.com>
2019-11-09 17:34:28 +0000

    Implementamos la parte de instanciar a los usuarios que se apuntan a un chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
-const Chat = () => (
-	<h1>Chat</h1>
-)
+let socket
+
+const Chat = ({ location }) => {
+	const [name, setName] = useState('')
+	const [room, setRoom] = useState('')
+	const ENDPOINT = 'localhost:5000' // ruta del server
+
+	useEffect(() => {
+		const { name, room } = queryString.parse(location.search) // Pasa los parametros del GET a object
+
+		socket = io(ENDPOINT) // Le damos la ruta del server al socket
+
+		// Seteamos los states
+		setName(name)
+		setRoom(room)
+
+		// Cuando alguien entra al chat, envia name y room al server
+		socket.emit('join', { name, room })
+	}, [ENDPOINT, location.search])
+
+	return (
+		<h1>Chat</h1>
+	)
+}
 
 export default Chat
